3aff4fccedf028a7628b8479c33c8511e0c83f3f
SemyonPozin <154556698+SemyonPozin@users.noreply.github.com>
2024-12-16 23:40:22 +0300

    Delete Documents/лабы directory

--- Documents/лабы/1 курс 2 сем/курсач/techplanet/src/Selectors/getGoods.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-const goodsState=(state)=>state.goods.goods;
-
-export const getGoods=createSelector(goodsState,
-    (goods)=>{return goods;}
-)
